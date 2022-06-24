9ca131c9694e97914a30374f22e42cee63a67615
“IsayenkaAndrei” <bmzigorim@yandex.ru>
2022-06-24 15:41:47 +0300

    feat: add implement revers Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  const absStrNum = String(Math.abs(n));
   
+
+  let result = ''; 
+
+  for (let i = absStrNum.length - 1; i >= 0; i--) {
+    i === absStrNum.length - 1 && absStrNum[i] === '0' ? result += '' : result += absStrNum[i];
+    }
+  
+  return parseInt(result);
 }
